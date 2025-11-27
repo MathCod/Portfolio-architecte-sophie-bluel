56a1b3d2f4ebf4de43b521b5f6c55a099a0fbd27
Mathias BERGER <mathias.codage@gmail.com>
2025-11-27 16:33:43 +0100

    Création du projet + ajout de la gallerie via JS

---
+++ FrontEnd/scripts/script.js
@@ -0,0 +1,26 @@
+fetch("http://localhost:5678/api/works")
+    .then(response => response.json())
+    .then(data => {
+        const gallery = document.querySelector(".gallery");
+
+        // Pour chaque "work" dans "data"
+        for (const work of data) {
+            
+            // Création des balises
+            const figure = document.createElement("figure");
+            const img = document.createElement("img");
+            const figcaption = document.createElement("figcaption");
+
+            // Remplir les informations
+            img.src = work.imageUrl; // L'url de l'image
+            img.alt = work.title;    // Le texte alternatif
+            figcaption.innerText = work.title; // Le titre sous l'image
+
+            // Assembler les balises
+            figure.appendChild(img);
+            figure.appendChild(figcaption);
+
+            // Mettre le tout dans "gallery"
+            gallery.appendChild(figure);
+        }
+    });
